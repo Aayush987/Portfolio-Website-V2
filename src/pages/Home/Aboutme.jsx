80977c24f2714bdb0912b5a30b43170a9b302e3a
Aayush <aayushsharma47163@gmail.com>
2023-12-25 18:33:13 +0530

    Bug Fixed

--- src/pages/Home/Aboutme.jsx
+++ src/pages/Home/Aboutme.jsx
@@ -1,8 +1,10 @@
+import  ProfilePhoto  from  "../../assets/me4.jpg";
+
 export default function Aboutme() {
     return (
       <section id="AboutMe" className="about--section">
         <div className="about--section--img">
-          <img src="../public/me4.jpg" alt="About Me" />
+          <img src= {ProfilePhoto} alt="About Me" />
         </div>
         <div className="hero--section--content--box about--section--box">
           <div className="hero--section--content">
